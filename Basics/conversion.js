664a8cce30c51796a36046aef1d5e749db4e0ad0
Rahul Kumar Gupta <121859773+Rahulgupta79@users.noreply.github.com>
2023-10-07 17:54:17 +0000

    conversion

---
+++ Basics/conversion.js
@@ -0,0 +1,37 @@
+let m=234
+// console.log(m)
+// console.log(typeof(m))
+let n=String(m)
+// console.log(n)
+// console.log(typeof(n))
+
+let o="Hi"
+// console.log(o)
+// console.log(typeof(o))
+// console.log("hi")
+let p=Number(o)
+// console.log(p)
+// console.log(typeof(p))
+
+// console.log(undefined)
+// console.log(typeof(undefined))
+
+// console.log(null)
+// console.log(typeof(null))
+
+let b=true
+// console.log(b)
+// console.log(typeof(b))
+// console.log(Number(b))
+
+let c=1
+// console.log(Boolean(c))
+
+let f=3
+let g=-f
+// console.log(g)
+
+// console.log("1"+2)
+// console.log(1+"2")
+// console.log("1"+2+2)
+// console.log(1+2+"2")
