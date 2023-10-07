e09113a7a42417f38feec76e2aab3dfe21258f6c
Rahul Kumar Gupta <121859773+Rahulgupta79@users.noreply.github.com>
2023-10-07 17:53:43 +0000

    variables

---
+++ Basics/variable.js
@@ -0,0 +1,13 @@
+console.log("Rahul Gupta");
+const id=7979;
+let v="rahul@gmail.com";
+var c=797975;
+// id=23232 not allowed on const type data
+console.log(id);
+v="magic";
+c="welcome";
+console.log(v);
+console.log(c);
+console.log(id,v,c)
+// prefer not to use var for variavle create because issue in block scope and functional scope
+// if you create a variable and not assign data in it then it store undefine 
